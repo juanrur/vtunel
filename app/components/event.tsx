fc1eda9c821a8e4889a004363c4ce5f28c2d7dd1
juanr <juanruizcampa@gmail.com>
2024-11-07 18:53:37 +0100

    trying

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -1,6 +1,13 @@
 'use client'
+import { useState, useRef, useCallback } from 'react'
+import { Resizable } from 're-resizable'
 
 export default function Event ({ name, height, margin, id }: { name: string, height: number, margin: number, id: string }) {
+  const [eventHeight, setEventHeight] = useState(height)
+  const resizableRef = useRef<Resizable>(null)
+
+  console.log(eventHeight)
+
   const handleDragStart = (event: DragEvent) => {
     event.dataTransfer?.setData('text/plain', id)
   }
@@ -8,12 +15,31 @@ export default function Event ({ name, height, margin, id }: { name: string, hei
   const handleDragEnd = (event) => {
   }
 
+  const handleResize = useCallback((e, direction, ref, d) => {
+    setEventHeight(height + d.height)
+  }, [height])
+
   return (
+    <Resizable
+      ref={resizableRef}
+      className='bg-blue-500 '
+      style={{ marginTop: margin + 'px' }}
+      defaultSize={{ width: '100%', height: eventHeight }}
+      minHeight={20}
+      enable={{ bottom: true }}
+      onResizeStop={handleResize}
+      handleStyles={{
+        bottom: {
+          bottom: 0,
+          height: 20,
+          cursor: 'ns-resize'
+        }
+      }}
+    >
     <button draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}
-    className='bg-blue-500'
-    style={{ height: height + 'px', marginTop: margin + 'px' }}
     >
       {name}
     </button>
+    </Resizable>
   )
 }
