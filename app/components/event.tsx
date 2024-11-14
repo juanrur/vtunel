ff9f15ea0aae2848e019ee0e2ed88cb426731e47
juanr <juanruizcampa@gmail.com>
2024-11-15 00:04:44 +0100

    delete re-resizable dependency and fix some errors

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -1,45 +1,25 @@
 'use client'
-import { useState, useRef, useCallback } from 'react'
-import { Resizable } from 're-resizable'
+import { useState } from 'react'
 
 export default function Event ({ name, height, margin, id }: { name: string, height: number, margin: number, id: string }) {
   const [eventHeight, setEventHeight] = useState(height)
-  const resizableRef = useRef<Resizable>(null)
 
-  console.log(eventHeight)
-
-  const handleDragStart = (event: DragEvent) => {
+  const handleDragStart = (event: any) => {
     event.dataTransfer?.setData('text/plain', id)
   }
 
-  const handleDragEnd = (event) => {
+  const handleDragEnd = () => {
   }
 
-  const handleResize = useCallback((e, direction, ref, d) => {
-    setEventHeight(height + d.height)
-  }, [height])
-
   return (
-    <Resizable
-      ref={resizableRef}
-      className='bg-blue-500 '
-      style={{ marginTop: margin + 'px' }}
-      defaultSize={{ width: '100%', height: eventHeight }}
-      minHeight={20}
-      enable={{ bottom: true }}
-      onResizeStop={handleResize}
-      handleStyles={{
-        bottom: {
-          bottom: 0,
-          height: 20,
-          cursor: 'ns-resize'
-        }
-      }}
-    >
-    <button draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}
+    <button
+      draggable
+      className='bg-blue-500'
+      style={{ marginTop: margin + 'px', width: '100%', height: eventHeight }}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
     >
       {name}
     </button>
-    </Resizable>
   )
 }
