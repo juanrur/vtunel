6ccf3692e49e160ca265e27feaaf857c8fa2e98f
Juan <juanruizcampa@gmail.com>
2025-05-12 21:46:21 +0200

    change data base to supabase

--- app/db/client.ts
+++ app/db/client.ts
@@ -1,77 +1,65 @@
 'use server'
 import { Day, Event } from '@/types'
-import { createClient } from '@libsql/client'
 import { revalidatePath } from 'next/cache'
-import { uuid } from 'uuidv4'
+import { createClient } from '@supabase/supabase-js'
 
-const client = createClient({
-  url: 'libsql://vtunel-events-system-juanr-12.turso.io',
-  authToken: process.env.DATABASE_TOKEN ?? ''
-})
+const supabase = createClient(process.env.DATABASE_URL ?? '', process.env.DATABASE_TOKEN ?? '')
 
 export async function getAllEvents (): Promise<Day> {
-  const eventsResponse = await client.execute('SELECT * FROM Events;')
-  return eventsResponse.rows.map<Event>((
-    { id, startTime, endTime, name }) => ({
-    id: String(id),
-    name: String(name),
-    startTime: new Date(String(startTime)),
-    endTime: new Date(String(endTime))
-  }))
+  const { data, error } = await supabase
+    .from('events')
+    .select('*')
+
+  if (error) console.error('Error fetching events:', error)
+
+  if (data) {
+    return data.map<Event>(({ id, startTime, endTime, name }) => (
+      {
+        id,
+        name,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime)
+      }
+    ))
+  } else return []
 }
 
 export async function fetchEvents (week : string|Date = 'now'): Promise<Event[]> {
-  // try {
-  //   const eventsResponse = await client.execute({
-  //     sql: "SELECT * FROM Events WHERE STRFTIME('%Y', startTime) = '2024' AND STRFTIME('%W', startTime) = STRFTIME('%W', ?);",
-  //     args: [typeof week === 'string' ? week : convertDateToSqlDate(week)]
-  //   })
+  const { data, error } = await supabase
+    .from('events')
+    .select('*')
 
-  //   return eventsResponse.rows.map<Event>((
-  //     { id, startTime, endTime, name }) => ({
-  //     id: String(id),
-  //     name: String(name),
-  //     startTime: new Date(String(startTime)),
-  //     endTime: new Date(String(endTime))
-  //   }))
-  // } catch (error) {
-  //   throw new Error('error' + error)
-  // }
-  console.log('fetchEvnts', week)
-  const date = new Date()
-  date.setHours(2, 0, 0, 0)
+  if (error) console.error('Error fetching events:', error)
 
-  const endDate = new Date(date)
-  endDate.setMinutes(endDate.getMinutes() + 30)
-
-  return [
-    {
-      id: '123124125',
-      name: 'ejemplo sin wifi',
-      startTime: date,
-      endTime: endDate
-    }
-  ]
+  if (data) {
+    return data.map<Event>(({ id, startTime, endTime, name }) => (
+      {
+        id,
+        name,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime)
+      }
+    ))
+  } else return []
 }
 
 export async function insertEvent ({ startTime, endTime, name } : Omit<Event, 'id'>) {
-  await client.execute({
-    sql: 'INSERT INTO Events (startTime, endTime, name, id) VALUES ($startTime, $endTime, $name, $id);',
-    args: {
-      startTime: convertDateToSqlDate(startTime),
-      endTime: convertDateToSqlDate(endTime),
-      name,
-      id: uuid()
-    }
-  })
+  const { data, error } = await supabase
+    .from('events')
+    .insert([
+      {
+        startTime: startTime.toISOString(),
+        endTime: endTime.toISOString(),
+        name
+      }
+    ])
+    .select()
+
+  if (error) {
+    console.error('Error inserting event:', error)
+  } else {
+    console.log('Inserted event:', data)
+  }
 
   revalidatePath('/')
-}
-
-const convertDateToSqlDate = (date : Date) => {
-  const [dayDate, time] = date.toISOString().split('T')
-  const [year, month, day] = dayDate.split('-')
-  const [hours, minutes, seconds] = time.split(':')
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
