22f66d6d0f817350ffbfec001bb35d5363b9decc
Juan Ruiz <ruiz.cajua25@triana.salesianos.edu>
2025-11-21 12:57:17 +0100

    feat: implement middleware for session verification and redirect to login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import React from 'react'
-import { redirect } from 'next/navigation'
-import { createServerClient } from '@/utils'
-import { cookies } from 'next/headers'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,10 +15,6 @@ export default async function RootLayout ({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const supabase = createServerClient(cookies)
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (session === null) redirect('/login')
   return (
     <html lang="en">
     <body className={`${inter.className} h-svh`}>{children}</body>
