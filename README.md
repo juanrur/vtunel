1e3b173aab1bae5d772c21ca8a7f6cda894e1683
Juan Ruiz <ruiz.cajua25@triana.salesianos.edu>
2025-11-21 10:50:26 +0100

    feat: update TaskPage component to improve event fetching and structure

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 - [ ] put z-index of 00:00 from hours column well
 - [ ] split week arrow and header to make header a server component
 - [ ] add drag style
+
+- [ ] poner algo que envuelva a los componentes para hacer el drag and drop y guardar toda la lógica del drag and drop hay
+- [ ] gestionar la repetición de eventos
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,22 +1,28 @@
 'use client'
-
+import SingOutButton from '@/components/sing-out-button'
 import { useEventsStore } from '@/store'
 import { useEffect } from 'react'
 
 export default function TaskPage () {
   const { events, getAllEvents, token } = useEventsStore()
   useEffect(() => {
-    getAllEvents(token)
-  }, [getAllEvents])
+    if (token) getAllEvents(token)
+  }, [getAllEvents, token])
+
   return (
-    <div className='flex items-center justify-center h-screen'>
-      {JSON.stringify(events)}
-      {events.map(event => (
-        <div key={event.id} className='p-4 m-2 border rounded shadow'>
-          <h2 className='text-xl font-bold'>{event.name}</h2>
-          <p className='text-gray-600'>{event.startTime.toISOString()}</p>
-        </div>
-      ))}
-    </div>
+    <main className='text-right'>
+      <div>
+        <SingOutButton />
+      </div>
+
+      <div className='flex items-center justify-center h-screen'>
+        {events.map(event => (
+          <div key={event.id} className='p-4 m-2 border rounded shadow'>
+            <h2 className='text-xl font-bold'>{event.name}</h2>
+            <p className='text-gray-600'>{event.startTime.toISOString()}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
