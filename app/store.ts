46601b865102d3e2e2487cb51a5b3898e1be995f
juanrur <juanruizcampa@gmail.com>
2026-01-14 14:19:44 +0100

    fix: cambiar vista predeterminada de 'month' a 'week' en el store de eventos

--- app/store.ts
+++ app/store.ts
@@ -30,7 +30,7 @@ export const useEventsStore = create<EventsStore>((set) => ({
   token: null,
   pixelsPerMinute: 1,
   minutesPerDivided: 60,
-  view: 'month',
+  view: 'week',
   changeDivisionsPerDay: (number: number) => set(() => ({ divisionsPerDay: number })),
 
   changeEventStartTime: async (newStartTime: Date, eventID: string) => {
