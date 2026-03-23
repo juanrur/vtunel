fab25d999f28a09caf1cf845f5920cbc04b2b5c5
Juan Ruiz <juanruizcampa@gmail.com>
2026-03-24 00:05:58 +0100

    fix: middleware uncommented

--- middleware.ts
+++ middleware.ts
@@ -2,36 +2,36 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { createServerClient as createServerClientSupabase } from '@supabase/ssr'
 
 export async function middleware (request: NextRequest) {
-  // const pathname = request.nextUrl.pathname
+  const pathname = request.nextUrl.pathname
 
-  // // No verificar sesión en /login y /auth
-  // if (pathname.startsWith('/login') || pathname.startsWith('/auth')) {
-  //   return NextResponse.next()
-  // }
+  // No verificar sesión en /login y /auth
+  if (pathname.startsWith('/login') || pathname.startsWith('/auth')) {
+    return NextResponse.next()
+  }
 
-  // // Para otras rutas, verificar sesión
-  // const supabase = createServerClientSupabase(
-  //   process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-  //   {
-  //     cookies: {
-  //       getAll () {
-  //         return request.cookies.getAll()
-  //       },
-  //       setAll (cookiesToSet) {
-  //         cookiesToSet.forEach(({ name, value, options }) => {
-  //           request.cookies.set(name, value)
-  //         })
-  //       }
-  //     }
-  //   }
-  // )
+  // Para otras rutas, verificar sesión
+  const supabase = createServerClientSupabase(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+    {
+      cookies: {
+        getAll () {
+          return request.cookies.getAll()
+        },
+        setAll (cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            request.cookies.set(name, value)
+          })
+        }
+      }
+    }
+  )
 
-  // const { data: { session } } = await supabase.auth.getSession()
+  const { data: { session } } = await supabase.auth.getSession()
 
-  // if (session === null) {
-  //   return NextResponse.redirect(new URL('/login', request.url))
-  // }
+  if (session === null) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
   return NextResponse.next()
 }
