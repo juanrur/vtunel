30061676d8847ac24a1064f0d273a529938fb7a5
Juan Ruiz <juanruizcampa@gmail.com>
2026-04-09 20:28:11 +0200

    fix: supabase migrations

--- supabase/migrations/20260402224035_change typeof id from int to uuid.sql
+++ supabase/migrations/20260402224035_change typeof id from int to uuid.sql
@@ -2,12 +2,17 @@ alter table "public"."templates" drop column "name";
 
 alter table "public"."templates" add column "title" text not null;
 
-alter table "public"."templates" alter column "id" set default gen_random_uuid();
+-- primero quitar identity
+alter table "public"."templates"
+  alter column "id" drop identity if exists;
 
-alter table "public"."templates" alter column "id" drop identity;
+-- luego cambiar tipo
+alter table "public"."templates"
+  alter column "id" set data type uuid using gen_random_uuid();
 
-alter table "public"."templates" alter column "id" set data type uuid using "id"::uuid;
+-- luego poner default
+alter table "public"."templates"
+  alter column "id" set default gen_random_uuid();
 
-alter table "public"."templates" alter column "user_id" drop default;
-
-
+alter table "public"."templates"
+  alter column "user_id" drop default;
