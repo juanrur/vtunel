ec955a094221decabd86f768f67bddf1fff6dac0
Juan Ruiz <juanruizcampa@gmail.com>
2026-05-26 21:58:46 +0200

    change: middleware to pocketbase

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,18 @@
 import { type NextRequest, NextResponse } from 'next/server'
-import { createServerClient as createServerClientSupabase } from '@supabase/ssr'
+import PocketBase from 'pocketbase'
 
 export async function middleware (request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  // No verificar sesión en /login y /auth
   if (pathname.startsWith('/login') || pathname.startsWith('/auth')) {
     return NextResponse.next()
   }
 
-  // Para otras rutas, verificar sesión
-  const supabase = createServerClientSupabase(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-    {
-      cookies: {
-        getAll () {
-          return request.cookies.getAll()
-        },
-        setAll (cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            request.cookies.set(name, value)
-          })
-        }
-      }
-    }
-  )
+  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL ?? '')
+  const cookieHeader = request.headers.get('cookie') || ''
+  pb.authStore.loadFromCookie(cookieHeader)
 
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (session === null) {
+  if (!pb.authStore.isValid || !pb.authStore.model) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
