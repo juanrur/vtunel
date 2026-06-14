48f9908147d0a6dc5c94344ecd54254e8dcf77ca
Juan Ruiz <juanruizcampa@gmail.com>
2026-06-14 21:53:53 +0200

    delete: old callback fix: middleware to pocketbase

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,16 @@ import PocketBase from 'pocketbase'
 export async function middleware (request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  if (pathname.startsWith('/login') || pathname.startsWith('/auth')) {
+  if (pathname.startsWith('/login')) {
     return NextResponse.next()
   }
 
-  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL ?? '')
+  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL || '')
+
   const cookieHeader = request.headers.get('cookie') || ''
   pb.authStore.loadFromCookie(cookieHeader)
 
-  if (!pb.authStore.isValid || !pb.authStore.model) {
+  if (!pb.authStore.isValid) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
