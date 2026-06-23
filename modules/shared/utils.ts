f7d90f02d2b35e67ceace7e19ba8012f6bafb1d9
Juan Ruiz <juanruizcampa@gmail.com>
2026-06-23 21:53:16 +0200

    fix: bug with localtim

--- modules/shared/utils.ts
+++ modules/shared/utils.ts
@@ -1,6 +1,4 @@
 export function convertToLocalTime (dateString?: string | null): Date {
   if (!dateString) return new Date()
-  const utcDate = new Date(dateString)
-  const localOffset = utcDate.getTimezoneOffset() * 60000
-  return new Date(utcDate.getTime() - localOffset)
+  return new Date(dateString)
 }
