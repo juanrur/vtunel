a5fa97ea35f341d4dc06ff2425195238dcbdfe85
Juan Ruiz <juanruizcampa@gmail.com>
2026-07-09 21:13:37 +0200

    fix: style bug with checked tasks

--- modules/ui/tasks/components/task.tsx
+++ modules/ui/tasks/components/task.tsx
@@ -18,7 +18,7 @@ export default function Task ({ task, handleEditClick }: { task: TaskType, handl
             onChange={(event) => updateTask(task.id, { done: event.target.checked })}
             className='peer hidden'
           />
-          <div className='w-5 h-5 border rounded grid place-content-center transition-colors peer-checked:bg-[var(--foreground-rgb)]'>
+          <div className='w-5 h-5 border rounded grid place-content-center transition-colors peer-checked:bg-green-500'>
             <svg viewBox='0 0 24 24' className='w-3.5 h-3.5 fill-primary opacity-0 peer-checked:opacity-100 transition-opacity'>
               <path d='M20.285 6.709a1 1 0 0 0-1.414-1.418l-9.192 9.205-4.192-4.205a1 1 0 0 0-1.414 1.418l5 5a1 1 0 0 0 1.414 0l10-10z' />
             </svg>
