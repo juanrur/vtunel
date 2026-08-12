dfe6f63fbfaa8d88532347567d05113f57c46805
juanrur <juanruizcampa@gmail.com>
2026-08-12 13:54:08 +0200

    fix: adjust z-index for BottomBar component to improve visibility

--- modules/ui/calendar/components/bottom-bar.tsx
+++ modules/ui/calendar/components/bottom-bar.tsx
@@ -6,7 +6,7 @@ interface BottomBarProps {
 
 export default function BottomBar ({ onOpenSheet }: BottomBarProps) {
   return (
-    <nav className='fixed bottom-5 left-20 right-20 bg-secondary rounded-xl border-t border-r border-primary flex items-center justify-around p-2 z-50 md:hidden'>
+    <nav className='fixed bottom-5 left-20 right-20 bg-secondary rounded-xl border-t border-r border-primary flex items-center justify-around p-2 z-10 md:hidden'>
       <div className='flex-1 py-2 text-center text-sm text-zinc-500'>
         Acción 2
       </div>
