e71de3a2bcdc2b345f48cb5b92d117d10f04964f
juanrur <juanruizcampa@gmail.com>
2026-08-12 14:36:09 +0200

    fix: add overflow-visible class to AddDialog component for improved layout

--- modules/ui/shared/components/add/add-dialog.tsx
+++ modules/ui/shared/components/add/add-dialog.tsx
@@ -21,7 +21,7 @@ const AddDialog = forwardRef<HTMLDialogElement, AddDialogProps>(
 
     return (
       <dialog
-        className='bg-primary border-2 rounded-lg p-4 shadow-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 relative'
+        className='bg-primary border-2 rounded-lg p-4 shadow-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 relative overflow-visible'
         ref={ref}
       >
         <h2 className='text-2xl font-bold mb-4 text-white capitalize'>{children}</h2>
